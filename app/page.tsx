d03f62999565c52eaf084fea769f4624c151fcbb
Alon Mlievski <alonmlievski@gmail.com>
2023-06-16 21:43:58 +0300

    custom filters finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { CustomFilter, Hero, SearchBar, CarCard } from "@/cmps/index";
 import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
 
 export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
@@ -26,8 +27,8 @@ export default async function Home({ searchParams }) {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
