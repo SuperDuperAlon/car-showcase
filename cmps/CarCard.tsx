4e958690c38c303c745844dd59513d4cafa34c22
Alon Mlievski <alonmlievski@gmail.com>
2023-06-16 09:52:29 +0300

    car images fetching

--- cmps/CarCard.tsx
+++ cmps/CarCard.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import { CustomButton, CarDetails } from "@/cmps/index";
 
 interface CarCardProps {
@@ -30,7 +30,7 @@ function CarCard({ car }: CarCardProps) {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
