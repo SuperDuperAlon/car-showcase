20fae2ad4b90033e3c6c72134d134382c384fafc
Alon Mlievski <alonmlievski@gmail.com>
2023-06-16 13:04:19 +0300

    add search params

--- cmps/SearchBar.tsx
+++ cmps/SearchBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import SearchManufacturer from "./SearchManufacturer";
 import Image from "next/image";
 
@@ -19,8 +20,39 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 function SearchBar() {
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
+  const router = useRouter();
 
-  const handleSearch = () => {};
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === "" && model === "") {
+      return alert("please fill the searchbar");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${window.location.pathname}?${searchParams
+      .toString()
+      .toLowerCase()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
